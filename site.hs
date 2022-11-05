783d1f64f0780914174d9b3216b0b4c86cf8923c
Yao Li <hnkfliyao@gmail.com>
2022-11-04 19:35:06 -0700

    Update courses.

--- site.hs
+++ site.hs
@@ -38,9 +38,17 @@ main = hakyllWith config $ do
             >>= loadAndApplyTemplate "templates/default.html" defaultContext
             >>= relativizeUrls
 
+    match "courses/*" $ do
+        route $ setExtension "html"
+        compile $ pandocCompiler
+            >>= loadAndApplyTemplate "templates/course.html"  defaultContext
+            >>= loadAndApplyTemplate "templates/default.html" defaultContext
+            >>= relativizeUrls
+
     match "index.html" $ do
         route idRoute
         compile pubCompiler
+        compile courseCompiler
 
     match "publication.html" $ do
         route idRoute
@@ -57,4 +65,17 @@ pubCompiler = do
   getResourceBody
     >>= applyAsTemplate pubCtx
     >>= loadAndApplyTemplate "templates/default.html" pubCtx
+    >>= relativizeUrls
+
+courseCompiler :: Compiler (Item String)
+courseCompiler = do
+  courses <- recentFirst =<< loadAll "courses/*"
+  papers  <- recentFirst =<< loadAll "papers/*"
+  let ctx =
+        listField "courses" defaultContext (return courses)
+        `mappend` listField "papers" defaultContext (return papers)
+        `mappend` defaultContext
+  getResourceBody
+    >>= applyAsTemplate ctx
+    >>= loadAndApplyTemplate "templates/default.html" ctx
     >>= relativizeUrls
